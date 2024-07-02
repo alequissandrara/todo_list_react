4ad0dbb1fccc22817a5fb3ff8a8b45779eb4785b
alequissandrara <alequissandrarayane123@gmail.com>
2024-07-02 12:48:05 -0300

    feat: adicionar biblioteca de ícones do Bootstrap, criar componentes TaskForm e TaskList, ajustar configurações de CSS
    - Adicionada a biblioteca de ícones do Bootstrap para utilização de ícones padronizados.
    - Criado o componente TaskForm para a adição e edição de tarefas, incluindo validação básica de formulário.
    - Implementado o componente TaskList para exibir a lista de tarefas, com funcionalidade de filtro e ordenação de tarefas.
    - Atualizados os estilos globais no arquivo app.module.css, incluindo ajustes nas margens e padding dos componentes e melhorias na responsividade do layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import styles from "./App.module.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>conteúdo...</h1>
+      <main className={styles.main}>
+        <div>
+          <h2>O que vc vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList />
+        </div>
+      </main>
       <Footer />
     </div>
   );
