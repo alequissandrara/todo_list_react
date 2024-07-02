d1a3834580ef58d30965bf0a06d4ce0b2905b469
alequissandrara <alequissandrarayane123@gmail.com>
2024-07-02 10:40:33 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={Styles.footer}>
+      <p>
+        <span>React + TS TODO</span> @ 2024
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
